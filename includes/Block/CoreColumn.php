12fc179430da46d641eeb3a6a38463918c3bb027
John Jeeves <john.jeeves@americaneagle.com>
2022-06-21 10:49:53 +0100

    - core columns and column blocks

---
+++ includes/Block/CoreColumn.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Class CoreColumn
+ *
+ * @package PhpBlockBuilders\Block
+ */
+
+declare( strict_types=1 );
+
+namespace PhpBlockBuilders\Block;
+
+use PhpBlockBuilders\BlockBase;
+
+/**
+ * Class CoreColumn
+ *
+ * @package PhpBlockBuilders\Block
+ */
+class CoreColumn extends BlockBase {
+
+	/**
+	 * The container block name.
+	 *
+	 * @var string
+	 */
+	public static string $block_name = 'core/column';
+
+	/**
+	 * Create a Core Columns Block
+	 *
+	 * @param  string $content The columns container.
+	 * @param  array  $attrs Block attributes.
+	 *
+	 * @return string
+	 */
+	public static function create( string $content = '', array $attrs = [] ): string {
+
+		$attrs     = self::get_block_attrs( $attrs );
+		$classname = $attrs['classname'] ?? 'wp-block-column';
+
+		$block_template = <<<'TEMPLATE'
+		<div class="%1$s">%2$s</div>
+		TEMPLATE;
+
+		$inner_content = sprintf(
+			$block_template,
+			\esc_attr( $classname ), // 1
+			\filter_block_kses_value( $content, 'post' ), // 2
+		);
+
+		$data = self::get_data(
+			$attrs,
+			[ trim( $inner_content ) ]
+		);
+
+		return serialize_block( $data );
+
+	}
+
+
+}
