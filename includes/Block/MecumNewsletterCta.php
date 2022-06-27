4fbe96d7b3553b8483484e06ac8762672b876c40
John Jeeves <john.jeeves@americaneagle.com>
2022-06-27 17:50:17 +0100

    - starts mecum newsletter cta block

---
+++ includes/Block/MecumNewsletterCta.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Class MecumNewsletterCta
+ *
+ * @package PhpBlockBuilders\Block
+ */
+
+declare( strict_types=1 );
+
+namespace PhpBlockBuilders\Block;
+
+use PhpBlockBuilders\BlockBase;
+
+/**
+ * Class MecumNewsletterCta
+ *
+ * @package PhpBlockBuilders\Block
+ */
+class MecumNewsletterCta extends BlockBase {
+
+	/**
+	 * The block name.
+	 *
+	 * @var string
+	 */
+	public static string $block_name = 'mecum/newsletter-cta';
+
+	/**
+	 * The block classname.
+	 *
+	 * @var string
+	 */
+	public static string $block_classname = 'wp-block-mecum-newsletter-cta';
+
+
+}
