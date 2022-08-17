3cf57643e931fe7a72e68bb431b06c4dfc449844
John Jeeves <john.jeeves@americaneagle.com>
2022-08-17 17:24:48 +0100

    - updates CoreCover.php and CoreMediaText.php to allow for custom urls (without post id links) to tbe provided to Image.php for image tag generation - updates CoreMediaText.php to follow CoreCover attr pattern for image details

--- includes/Block/CoreCover.php
+++ includes/Block/CoreCover.php
@@ -37,22 +37,24 @@ class CoreCover extends BlockBase {
 	 * Create a Core Cover Block
 	 *
 	 * @param  string $content  Any cover block content text content (as blocks).
-	 * @param  array  $attrs  Block attributes ['id' => 'WP Attachment ID', 'dim_ratio' => 'dimRatio', 'object_fit' => 'image data object fit'].
+	 * @param  array  $attrs  Block attributes ['id' => 'WP Attachment ID', 'dim_ratio' => 'dimRatio', 'object_fit' =>
+	 *     'image data object fit'].
 	 *
 	 * @return string
 	 */
 	public static function create( string $content = '', array $attrs = [] ): string {
-
 		$data                  = self::get_data( $attrs );
-		$image_id              = absint( $attrs['id'] );
+		$image_id              = absint( $attrs['attrs']['id'] ?? 0 );
 		$dim_ratio             = $attrs['attrs']['dimRatio'] ?? 56;
 		$dim_ratio_closest_ten = ceil( $dim_ratio / 10 ) * 10;
 		$span_class            = $attrs['span_class'] ?? 'has-background-dim-' . $dim_ratio_closest_ten . ' wp-block-cover__gradient-background has-background-dim';
 		$image                 = Image::create(
 			$image_id,
 			[
-				'classname'   => sprintf( 'wp-block-cover__image-background wp-image-%s', $image_id ),
+				'classname'   => $image_id ? sprintf( 'wp-block-cover__image-background wp-image-%s', $image_id ) : 'wp-block-cover__image-background',
 				'image_attrs' => [ 'data-object-fit' => $attrs['object_fit'] ?? 'cover' ],
+				'url'         => $data['attrs']['url'] ?? '',
+				'alt'         => $data['attrs']['alt'] ?? '',
 			]
 		);
 
@@ -75,8 +77,11 @@ class CoreCover extends BlockBase {
 
 		$data['innerContent']      = [ $inner_content ];
 		$data['attrs']['dimRatio'] = $dim_ratio;
-		$data['attrs']['id']       = $image_id;
-		$data['attrs']['url']      = $image['attrs']['mediaLink'];
+		if ( 0 === $image_id ) {
+			unset( $data['attrs']['id'], $data['attrs']['className'] );
+		}
+
+		$data['attrs']['url'] = $image['attrs']['mediaLink'];
 
 		return serialize_block( $data );
 
